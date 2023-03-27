0d1f546ef98c6a6a6587dd8b9515c3174529085c
Sam Wells <sfwells0518@gmail.com>
2023-03-27 15:15:17 -0500

    create and test num generator route and create page_counter route

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,14 +1,33 @@
 class MyExamplesController < ApplicationController
   def random_fortune
     # The page should return a random fortune. Every time you refresh the page, you’ll get a new fortune. You should make up at least 3 different fortunes.
-    render json: { message: "testing testing" }
+    fortunes = ["you will be wise", "you will be fortunate", "you will learn how to predict the future"]
+    render json: { fortune: fortunes.sample }
   end
 
   def num_gen
-    # The page should return 6 randomly generated numbers between 1 and 60 so as to predict the upcoming lotto numbers.
+    winning_numbers = []
+    6.times do
+      winning_numbers << rand(1..60)
+    end
+    p winning_numbers
+    render json: { winning_numbers: winning_numbers }
+
+    # render json: { message: "hello" }
+
+    #  do message: hello for initial testing
+    #  The page should return 6 randomly generated numbers between 1 and 60 so as to predict the upcoming lotto numbers.
+    #  figure out how to randomly generate a number
+    #  do that 6 times
+    #  collect all 6 numbers
+    #  render that for the user
   end
 
-  def page_visits
+  def page_counter
+    count = 0
+    count += 1
+
+    render json: { count: count }
     # Every time a user visits/refreshes the page, the page should tell the user the number of visits made to that page so far. NOTE: This is impossible, but you should try your hand at this for a few minutes to discover why it’s impossible.
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "/fortune", controller: "my_examples", action: "random fortune"
-  get "/num_gen", controller: "my_examples", action: "random numbers"
-  get "/page_visits", controller: "my_examples", action: "page visits"
+  get "/fortune", controller: "my_examples", action: "random_fortune"
+  get "/num_gen", controller: "my_examples", action: "num_gen"
+  get "/page_counter", controller: "my_examples", action: "page_counter"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
