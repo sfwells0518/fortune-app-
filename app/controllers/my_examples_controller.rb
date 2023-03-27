525d7d3505e2fad847b46c35f714c92dc8d183d5
Sam Wells <sfwells0518@gmail.com>
2023-03-27 14:07:54 -0500

    add new routes for fortune, random numbers, page visits

---
+++ app/controllers/my_examples_controller.rb
@@ -0,0 +1,13 @@
+class MyExamplesController < ApplicationController
+  def random_fortune
+    # The page should return a random fortune. Every time you refresh the page, you’ll get a new fortune. You should make up at least 3 different fortunes.
+  end
+
+  def random_num
+    # The page should return 6 randomly generated numbers between 1 and 60 so as to predict the upcoming lotto numbers.
+  end
+
+  def page_visits
+    # Every time a user visits/refreshes the page, the page should tell the user the number of visits made to that page so far. NOTE: This is impossible, but you should try your hand at this for a few minutes to discover why it’s impossible.
+  end
+end
